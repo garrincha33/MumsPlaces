76bfe6ae9f216a9fd249f9c12699fc532d166649
Richard Price <mrspondooli@gmail.com>
2020-10-25 06:30:02 +0000

    inital commit

---
+++ MumsPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MumsPlaces
+//
+//  Created by Richard Price on 25/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
